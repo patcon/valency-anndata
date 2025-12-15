63b501ceefbd53aead97500a512e8a299f5ead16
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-12-15 00:35:56 -0500

    Added first-pass QC method.

--- debug.py
+++ debug.py
@@ -1,7 +1,7 @@
 import valency_anndata as val
 
-adata = val.datasets.polis.load("https://pol.is/report/r2dfw8eambusb8buvecjt") # small
-# adata = val.datasets.polis.load("https://pol.is/report/r29kkytnipymd3exbynkd", translate_to="en") # Chile
+# adata = val.datasets.polis.load("https://pol.is/report/r2dfw8eambusb8buvecjt") # small
+adata = val.datasets.polis.load("https://pol.is/report/r29kkytnipymd3exbynkd", translate_to=None) # Chile
 # adata = val.datasets.polis.load("https://pol.is/3ntrtcehas")
 
 print(adata)
@@ -13,6 +13,7 @@ print(adata.var)
 
 # val.datasets.polis.translate_statements(adata, translate_to="ja")
 val.preprocessing.impute(adata, strategy="mean")
-val.tools.pca(adata, layer="X_imputed_mean")
-val.scanpy.pl.pca(adata)
+val.preprocessing.calculate_qc_metrics(adata, inplace=True)
+# val.tools.pca(adata, layer="X_imputed_mean")
+# val.scanpy.pl.pca(adata)
 print(adata)
