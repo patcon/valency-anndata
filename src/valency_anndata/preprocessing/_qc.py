3c8c36c89a8747e519dc077a044c95113d58d3ff
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-12-16 01:25:37 -0500

    Whitespace fixes.

--- src/valency_anndata/preprocessing/_qc.py
+++ src/valency_anndata/preprocessing/_qc.py
@@ -13,13 +13,13 @@ def describe_obs(X: Matrix, obs_names: Optional[pd.Index] = None) -> pd.DataFram
         X = X.toarray()
     n_obs, n_var = X.shape
     mask_seen = ~np.isnan(X)
-    
+
     n_votes = mask_seen.sum(axis=1)
     n_agree = ((X == 1) & mask_seen).sum(axis=1)
     n_disagree = ((X == -1) & mask_seen).sum(axis=1)
     n_pass = ((X == 0) & mask_seen).sum(axis=1)
     n_engaged = n_agree + n_disagree
-    
+
     pct_agree = np.divide(n_agree, n_votes, out=np.zeros_like(n_agree, dtype=float), where=n_votes>0)
     pct_disagree = np.divide(n_disagree, n_votes, out=np.zeros_like(n_disagree, dtype=float), where=n_votes>0)
     pct_pass = np.divide(n_pass, n_votes, out=np.zeros_like(n_pass, dtype=float), where=n_votes>0)
@@ -27,7 +27,7 @@ def describe_obs(X: Matrix, obs_names: Optional[pd.Index] = None) -> pd.DataFram
     pct_agree_engaged = np.divide(n_agree, n_engaged, out=np.zeros_like(n_agree, dtype=float), where=n_engaged>0)
     pct_seen = n_votes / n_var
     mean_vote = np.divide(np.nansum(X, axis=1), n_votes, out=np.zeros_like(n_votes, dtype=float), where=n_votes>0)
-    
+
     return pd.DataFrame(
         {
             "n_votes": n_votes,
@@ -54,13 +54,13 @@ def describe_var(X: Matrix, var_names: Optional[pd.Index] = None) -> pd.DataFram
     X = X.T  # transpose for statements
     n_var, n_obs = X.shape
     mask_seen = ~np.isnan(X)
-    
+
     n_votes = mask_seen.sum(axis=1)
     n_agree = ((X == 1) & mask_seen).sum(axis=1)
     n_disagree = ((X == -1) & mask_seen).sum(axis=1)
     n_pass = ((X == 0) & mask_seen).sum(axis=1)
     n_engaged = n_agree + n_disagree
-    
+
     pct_agree = np.divide(n_agree, n_votes, out=np.zeros_like(n_agree, dtype=float), where=n_votes>0)
     pct_disagree = np.divide(n_disagree, n_votes, out=np.zeros_like(n_disagree, dtype=float), where=n_votes>0)
     pct_pass = np.divide(n_pass, n_votes, out=np.zeros_like(n_pass, dtype=float), where=n_votes>0)
@@ -68,7 +68,7 @@ def describe_var(X: Matrix, var_names: Optional[pd.Index] = None) -> pd.DataFram
     pct_agree_engaged = np.divide(n_agree, n_engaged, out=np.zeros_like(n_agree, dtype=float), where=n_engaged>0)
     pct_seen = n_votes / n_obs
     mean_vote = np.divide(np.nansum(X, axis=1), n_votes, out=np.zeros_like(n_votes, dtype=float), where=n_votes>0)
-    
+
     return pd.DataFrame(
         {
             "n_votes": n_votes,
@@ -99,10 +99,10 @@ def calculate_qc_metrics(
         raise ValueError("adata.X is None")
     obs_metrics = describe_obs(X, obs_names=adata.obs_names)
     var_metrics = describe_var(X, var_names=adata.var_names)
-    
+
     if inplace:
         adata.obs[obs_metrics.columns] = obs_metrics
         adata.var[var_metrics.columns] = var_metrics
         return None
-    
+
     return obs_metrics, var_metrics
