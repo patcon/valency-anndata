01c60b0a004366e5dd486011cc5ba66fe8f23efd
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-12-17 22:32:11 -0500

    Debug update.

--- debug.py
+++ debug.py
@@ -1,9 +1,10 @@
 import valency_anndata as val
-import numpy as np
 
 # adata = val.datasets.polis.load("https://pol.is/report/r2dfw8eambusb8buvecjt") # small
 adata = val.datasets.polis.load("https://pol.is/report/r29kkytnipymd3exbynkd", translate_to=None) # Chile
 # adata = val.datasets.polis.load("https://pol.is/3ntrtcehas")
+# adata = val.datasets.polis.load("https://pol.is/89euwydkce") # australia solar
+adata = val.datasets.polis.load("https://pol.is/2demo") # minimum wage
 
 print(adata)
 if adata.X is not None:
@@ -12,9 +13,5 @@ print(adata.X)
 print(adata.uns["schema"])
 print(adata.var)
 
-# val.datasets.polis.translate_statements(adata, translate_to="ja")
-if not isinstance(adata.X, np.ndarray):
-    raise TypeError("valency-anndata Polis pipeline assumes dense adata.X")
-
-val.tools.recipe_polis(adata, key_added="X_polis")
-val.scanpy.pl.embedding(adata, basis="polis")
+val.tools.recipe_polis(adata, key_added_pca="X_pca", key_added_kmeans="kmeans_polis")
+val.scanpy.pl.pca(adata, color="kmeans_polis")
