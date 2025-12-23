db21636b60bd4398308aa39ead81ce6c3abd587e
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-12-22 19:22:38 -0500

    Added stub pacmap tool.

--- src/valency_anndata/tools/__init__.py
+++ src/valency_anndata/tools/__init__.py
@@ -3,11 +3,13 @@ from scanpy.tools._tsne import tsne
 from scanpy.tools._umap import umap
 from scanpy.tools._leiden import leiden
 from ._kmeans import kmeans
+from ._pacmap import pacmap
 from ._polis import recipe_polis
 
 
 __all__ = [
     "kmeans",
+    "pacmap",
     "recipe_polis",
 
     # Simple re-export of scanpy.
